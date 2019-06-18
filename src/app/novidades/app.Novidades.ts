a7d5ab4f9c35cb870ce2c65b68c10171a5c1dd33
Allan Andrade de Jesus <42648038+Allan93aj@users.noreply.github.com>
2019-06-18 10:23:11 -0300

    versão 1.0

---
+++ src/app/novidades/app.Novidades.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.novidades.html',
+  styleUrls: ['./app.novidades.css']
+})
+export class AppComponent {
+  title = 'aula-angular';
+}
