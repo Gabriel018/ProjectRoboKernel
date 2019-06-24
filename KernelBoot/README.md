d0000a57aa47b5287b0a48aa6b03aea5b5549ce3
GabrielDuke <42377719+Gabriel018@users.noreply.github.com>
2019-06-23 22:20:18 -0300

    Boot v,1

---
+++ KernelBoot/README.md
@@ -0,0 +1,21 @@
+# ChatBot com DialogFlow
+
+
+### Projeto desenvolvido em Angular 7 com DialogFlow 
+
+[![](https://cdn-images-1.medium.com/max/800/0*KHLiJyA5vxzlERXW)](https://cdn-images-1.medium.com/max/800/0*KHLiJyA5vxzlERXW)
+
+
+
+
+#### Tutorial 
+[Parte 1]( https://medium.com/@kheronn.machado/aplicacao-angular7-dados-abertos-part1-ba6b87573f86 )
+
+[Parte 2]( https://medium.com/@kheronn.machado/chatbot-angular7-dialogflow-part1-16abd26cce62)
+
+
+
+Baixe o arquivo e na pasta do projeto, execute no terminal
+`npm install`
+
+
---
+++ KernelBoot/src/app/boot.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BootService {
+
+  private baseURL: string = "https://api.dialogflow.com/v1/query?v=20150910";
+  private token: string = 'aae58bef808241238ac9b4a0eac61bbf'
+
+  constructor(private http: HttpClient) { }
+
+  public getResponse(query: string) {
+    let data = {
+      query: query,
+      lang: 'en',
+      sessionId: '12345'
+    }
+    return this.http
+      .post(`${this.baseURL}`, data, { headers: this.getHeaders() })
+
+
+  }
+
+  public getHeaders() {
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', `Bearer ${this.token}`);
+    return headers;
+  }
+}
