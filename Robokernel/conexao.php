57f699ecc07c2d54cb1c3b30a0ce27f8d4469f75
GabrielDuke <42377719+Gabriel018@users.noreply.github.com>
2019-07-04 16:02:26 -0300

    Add files via upload

---
+++ Robokernel/conexao.php
@@ -0,0 +1,10 @@
+<?php
+$servidor = 'localhost';
+$usuario  = 'root';
+$senha 	  = '';
+$banco 	  = 'robokernel';
+//mysqli_connect serve para conectar os arquivos php bd
+$conexao  = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+
+?>
