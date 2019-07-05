d55ed9627a32559f11c814596fc03d9d580f16b8
maronfranc <41963031+maronfranc@users.noreply.github.com>
2019-07-05 17:59:42 -0300

    Add files via upload

--- Robokerneel/index.php
+++ Robokerneel/index.php
@@ -1,5 +1,3 @@
-
-
 <?php
 include('conexao.php');
 $sql = "select * from produto";
@@ -7,32 +5,33 @@ $sql2 = "SELECT * FROM produto";
 $resultado2 = mysqli_query($conexao,$sql2);
 $resultado = mysqli_query($conexao,$sql);
 include 'header.php';
+session_start();
 ?>
 
 <!--------------------------HEADER-------------------------------->
 
 
-<div class="container header"> 
+<div class="container header">
   <div class="row">
     <div class="col-lg-3 logo">
       <img src="./img/logo1.png" alt="">
     </div>
-    
+
     <div class="col-lg-3 loja-fisica">
       <p>LOJA FÍSICA <br>
-      R. Santa Luzia, 735 - Sala 504 <br>
-      Rio de Janeiro / RJ</p>
+        R. Santa Luzia, 735 - Sala 504 <br>
+        Rio de Janeiro / RJ</p>
     </div>
-    
+
     <div class="col-lg-3 cetral-atendimento">
       <p>CENTRAL DE ATENDIMENTO <br>
         (21) 1234-5678 <br>
-          info@robokernel.net</p>
+        info@robokernel.net</p>
     </div>
-    
+
     <div class="col-lg-3 horario-funcionamento">
       <p>HORÁRIO DE FUNCIONAMENTO <br>
-      De segunda à sexta das 9:00 às 18:30</p>
+        De segunda à sexta das 9:00 às 18:30</p>
     </div>
   </div>
 </div><br>
@@ -51,146 +50,181 @@ include 'header.php';
         <a href="carrinho.php"><i class="fas fa-shopping-cart"></i></a>
       </div>
     </div>
-  </div>    
+  </div>
 </div><br>
 
 <!----------------------------MENU-LATERAL------------------------>
 
 <div class="container menu-lateral">
   <div class="row">
-      <div class="col-lg-3 lateral" >
-        <ul class="list-group">
-        <a href="novidades.php"><li class="list-group-item">Novidades</li></a>
-        <a href="#"><li class="list-group-item">Placas Arduinos</li></a>
-        <a href="#"><li class="list-group-item">Shields para Arduino</li></a>
-        <a href="#"><li class="list-group-item">Automação Resid.</li></a>
-        <a href="#"><li class="list-group-item">Embarcados</li></a>
-        <a href="#"><li class="list-group-item">Internet das coisas</li></a>
-        <a href="#"><li class="list-group-item">Cabos e Conectores</li></a>
-        <a href="#"><li class="list-group-item">Displays e LCDs</li></a>
-        <a href="#"><li class="list-group-item">Drivers de Motores</li></a>
-        <a href="#"><li class="list-group-item">Fontes e Baterias</li></a>
-        <a href="#"><li class="list-group-item">Itens Eletrônicos</li></a>
-        <a href="#"><li class="list-group-item">Itens Mecanico</li></a>
-        <a href="#"><li class="list-group-item">Motores</li></a>
-        <a href="#"><li class="list-group-item">Protoboards</li></a>
-        <a href="#"><li class="list-group-item">Rodas</li></a>
-        <a href="#"><li class="list-group-item">Sensores</li></a>
-        <a href="#"><li class="list-group-item">Servos & R/C</li></a>
-        <a href="#"><li class="list-group-item">Wireless</li></a>
-        </ul>
+    <div class="col-lg-3 lateral">
+      <ul class="list-group">
+        <a href="novidades.php">
+          <li class="list-group-item">Novidades</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Placas Arduinos</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Shields para Arduino</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Automação Resid.</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Embarcados</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Internet das coisas</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Cabos e Conectores</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Displays e LCDs</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Drivers de Motores</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Fontes e Baterias</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Itens Eletrônicos</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Itens Mecanico</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Motores</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Protoboards</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Rodas</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Sensores</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Servos & R/C</li>
+        </a>
+        <a href="#">
+          <li class="list-group-item">Wireless</li>
+        </a>
+      </ul>
+    </div>
+
+    <!---------------------------CONTEUDO-PRINCIPAL----------------------------->
+
+    <div class="col-lg-9 principal" style="padding:0px;">
+      <div id="carousel-example-generic" class="carousel slide" style="width:98%;">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <div class="item active">
+            <img src="./img/robotica.jpg" alt="...">
+          </div>
+
+          <div class="item">
+            <img src="./img/robotica.jpg" alt="...">
+          </div>
+
+          <div class="item">
+            <img src="./img/robotica.jpg" alt="...">
+          </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+          <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+          <span class="icon-next"></span>
+        </a>
       </div>
 
-<!---------------------------CONTEUDO-PRINCIPAL----------------------------->
+      <script>
+        $('.carousel').carousel({
+          interval: 2000
+        })
+      </script>
+      <br><br>
 
-        <div class="col-lg-9 principal" style="padding:0px;"> 
-        <div id="carousel-example-generic" class="carousel slide"style="width:98%;">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
-
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner">
-    <div class="item active">
-      <img src="./img/robotica.jpg" alt="...">
-    </div>
-
-    <div class="item">
-      <img src="./img/robotica.jpg" alt="...">
-    </div>
-
-    <div class="item">
-      <img src="./img/robotica.jpg" alt="...">
-    </div>
-  </div>
-  
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-    <span class="icon-prev"></span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-    <span class="icon-next"></span>
-  </a>
-</div>
-
-<script>
-  $('.carousel').carousel({
-  interval: 2000
-})
-</script>
-    <br><br>  
-
-    <!--PRODUTOS-NOVIDADES-->
-    <div class="container prod-novidades">
-      <div class="row">
-        <div class="col-sm-12">
-    <h3>Novidades</h3>
-    <hr>
-       <?php
+      <!--PRODUTOS-NOVIDADES-->
+      <div class="container prod-novidades">
+        <div class="row">
+          <div class="col-sm-12">
+            <h3>Novidades</h3>
+            <hr>
+            <?php
          $i = 1;
           do {
             ($dados = mysqli_fetch_array($resultado)); 
 		?>
-                
-       <div class="col-md-2 container-fluid novidades">
-          <div class="thumbnail">
-             <a href="produtop.php?codproduto=<?= $dados['codproduto']; ?>">
-             <img src="fotos/<?= $dados['imagem']; ?>" alt="" class="img-fluid"></a>
-          </div>
-          <div class="descricao">
-           <h5><?php echo $dados['descricao']; ?></h5>
-           <p><?php echo $dados['categoria']; ?></p>
-			<h4><?php echo "R$ ";
-			echo $dados['preco']; ?>
-            </h4>
-          </div>
-      </div>
-     <?php
+
+            <div class="col-md-2 container-fluid novidades">
+              <div class="thumbnail">
+                <a href="produtop.php?codproduto=<?= $dados['codproduto']; ?>">
+                  <img src="fotos/<?= $dados['imagem']; ?>" alt="" class="img-fluid"></a>
+              </div>
+              <div class="descricao">
+                <h5><?php echo $dados['descricao']; ?></h5>
+                <p><?php echo $dados['categoria']; ?></p>
+                <h4><?php
+            echo "R$ ";
+            echo $dados['preco']; ?>
+                </h4>
+              </div>
+            </div>
+            <?php
         $i++;
          } while ($i<6);
      ?>
-      </div>
+          </div>
         </div>
-    </div>
+      </div>
 
 
-    <!--MAIS-VENDIDOS-->
-    <div class="container prod-maisvendido">
-      <div class="row">
-        <div class="col-sm-12">
-    <h3>Mais Vendidos </h3>
-    <hr>
-       <?php
+      <!--MAIS-VENDIDOS-->
+      <div class="container prod-maisvendido">
+        <div class="row">
+          <div class="col-sm-12">
+            <h3>Mais Vendidos </h3>
+            <hr>
+            <?php
          $i = 1;
           do {
             ($dados = mysqli_fetch_array($resultado)); 
 		?>
-                
-       <div class="col-md-2 container-fluid mais-vendidos">
-          <div class="thumbnail">
-             <a href="produtop.php?codproduto=<?= $dados['codproduto']; ?>">
-             <img src="fotos/<?= $dados['imagem']; ?>" alt="" class="img-fluid"></a>
+
+            <div class="col-md-2 container-fluid mais-vendidos">
+              <div class="thumbnail">
+                <a href="produtop.php?codproduto=<?= $dados['codproduto']; ?>">
+                  <img src="fotos/<?= $dados['imagem']; ?>" alt="" class="img-fluid"></a>
+              </div>
+              <div class="descricao">
+                <h5><?php echo $dados['descricao']; ?></h5>
+                <p><?php echo $dados['categoria']; ?></p>
+                <p><?php
+                  echo "R$ ";
+                  echo $dados['preco'];
+                ?></p>
+              </div>
+            </div>
+            <?php
+              $i++;
+              } while ($i<6);
+            ?>
           </div>
-          <div class="descricao">
-           <h5><?php echo $dados['descricao']; ?></h5>
-           <p><?php echo $dados['categoria']; ?></p>
-			<p><?php echo "R$ ";
-			echo $dados['preco']; ?>
-            </p>
-          </div>
-      </div>
-     <?php
-        $i++;
-         } while ($i<6);
-     ?>
-      </div>
         </div>
-    </div>
+      </div>
 
-<?php include "footer.php";?>
-
-
+      <?php include "footer.php";?>
