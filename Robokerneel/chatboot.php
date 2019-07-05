6c8cd7130da630bf8bd7d1aeb47ac7a8459998ae
GabrielDuke <42377719+Gabriel018@users.noreply.github.com>
2019-07-05 19:13:18 -0300

    modificaçoes

--- Robokerneel/chatboot.php
+++ Robokerneel/chatboot.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link href="Estilo.css" rel="stylesheet">
+    <link href="css\boot.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
