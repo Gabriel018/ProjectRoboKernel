0913f7fca79781a69f931c11e8b7ac1769a2f867
GabrielDuke <42377719+Gabriel018@users.noreply.github.com>
2019-07-09 21:47:37 -0300

    footer

--- Robokerneel/footer.php
+++ Robokerneel/footer.php
@@ -1,6 +1,6 @@
 ﻿<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
 <link rel="stylesheet" href="css/footer.css">
-
+</div></div>
 <footer class="footer">
   <div class="container">
 		<div class="row" style="margin: 0;">
